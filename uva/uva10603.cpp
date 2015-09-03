59ca95a80c6d8bb7bb72b6b2b0b911c328500722
Jiege Chen <jiegec@qq.com>
2015-09-03 12:18:32 +0800

    uva10603 uva10798

---
+++ uva/uva10603.cpp
@@ -0,0 +1,132 @@
+#include <stdio.h>
+#include <string.h>
+#include <queue>
+
+using namespace std;
+
+struct state {
+  int x;
+  int y;
+  int z;
+  int sum;
+};
+
+bool operator <(state a,state b) {
+  return a.sum > b.sum;
+}
+
+const int MAX = 0xfffffff;
+
+int minsum[210][210][210];
+int mind[210];
+
+int main() {
+  int t,a,b,c,d;
+  scanf("%d",&t);
+  while(t--) {
+    scanf("%d%d%d%d",&a,&b,&c,&d);
+    for (int i = 0;i <= a+1;i++)
+      for (int j = 0;j <= b+1;j++)
+        for (int k = 0;k <= c+1;k++)
+          minsum[i][j][k] = MAX;
+    for (int i = 0;i < 201;i++)
+      mind[i] = MAX;
+    state start;
+    start.x = start.y = 0;
+    start.z = c;
+    start.sum = 0;
+    queue<state> pq;
+    minsum[0][0][c] = 0;
+    mind[c] = 0;
+    mind[0] = 0;
+    pq.push(start);
+    while (!pq.empty()) {
+      state current = pq.front();
+      pq.pop();
+      mind[current.x] = min(mind[current.x],current.sum);
+      mind[current.y] = min(mind[current.y],current.sum);
+      mind[current.z] = min(mind[current.z],current.sum);
+      if (current.sum > mind[d]) {
+        continue;
+      }
+
+      int xtoy = min(current.x,b-current.y);
+      if (xtoy > 0) {
+        state temp = current;
+        temp.x -= xtoy;
+        temp.y += xtoy;
+        temp.sum += xtoy;
+        if (minsum[temp.x][temp.y][temp.z] > temp.sum) {
+          minsum[temp.x][temp.y][temp.z] = temp.sum;
+          pq.push(temp);
+        }
+      }
+
+      int ytoz = min(current.y,c-current.z);
+      if (ytoz > 0) {
+        state temp = current;
+        temp.y -= ytoz;
+        temp.z += ytoz;
+        temp.sum += ytoz;
+        if (minsum[temp.x][temp.y][temp.z] > temp.sum) {
+          minsum[temp.x][temp.y][temp.z] = temp.sum;
+          pq.push(temp);
+        }
+      }
+
+      int ztox = min(current.z,a-current.x);
+      if (ztox > 0) {
+        state temp = current;
+        temp.z -= ztox;
+        temp.x += ztox;
+        temp.sum += ztox;
+        if (minsum[temp.x][temp.y][temp.z] > temp.sum) {
+          minsum[temp.x][temp.y][temp.z] = temp.sum;
+          pq.push(temp);
+        }
+      }
+
+      int xtoz = min(current.x,c-current.z);
+      if (xtoz > 0) {
+        state temp = current;
+        temp.x -= xtoz;
+        temp.z += xtoz;
+        temp.sum += xtoz;
+        if (minsum[temp.x][temp.y][temp.z] > temp.sum) {
+          minsum[temp.x][temp.y][temp.z] = temp.sum;
+          pq.push(temp);
+        }
+      }
+
+      int ytox = min(current.y,a-current.x);
+      if (ytox > 0) {
+        state temp = current;
+        temp.y -= ytox;
+        temp.x += ytox;
+        temp.sum += ytox;
+        if (minsum[temp.x][temp.y][temp.z] > temp.sum) {
+          minsum[temp.x][temp.y][temp.z] = temp.sum;
+          pq.push(temp);
+        }
+      }
+
+      int ztoy = min(current.z,b-current.y);
+      if (ztoy > 0) {
+        state temp = current;
+        temp.z -= ztoy;
+        temp.y += ztoy;
+        temp.sum += ztoy;
+        if (minsum[temp.x][temp.y][temp.z] > temp.sum) {
+          minsum[temp.x][temp.y][temp.z] = temp.sum;
+          pq.push(temp);
+        }
+      }
+    }
+    for (int i = d;i >= 0;i--) {
+      if (mind[i] != MAX) {
+        printf("%d %d\n", mind[i], i);
+        break;
+      }
+    }
+  }
+}
---
+++ uva/uva10798.cpp
@@ -0,0 +1,72 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <algorithm>
+#include <queue>
+
+using namespace std;
+
+struct state {
+  int x;
+  int y;
+  int up;
+  int right;
+  int down;
+  int left;
+  int max;
+};
+
+bool operator <(state a, state b) {
+  return a.max > b.max;
+}
+
+const int d[4][2] = {{1, 0}, {-1, 0}, {0, -1}, {0, 1}};
+bool visit[50][50][12][12][12][12];
+
+int main() {
+  int n;
+  char map[50][50] = {{'.'}};
+  while (scanf("%d\n",&n) && n) {
+    memset(visit,0,sizeof(visit));
+    state start;
+    for (int i = 0;i < n;i++) {
+      for (int j = 0;j < n;j++) {
+        scanf("%c",&map[i][j]);
+        if (map[i][j] == 'P') {
+          start.x = i;
+          start.y = j;
+        }
+      }
+      scanf("\n");
+    }
+    start.up = start.right = start.down = start.left = 0;
+    start.max = 0;
+    visit[start.x][start.y][0][0][0][0] = true;
+    priority_queue<state> pq;
+    pq.push(start);
+    while (!pq.empty()) {
+      state current = pq.top();
+      pq.pop();
+      if (current.x == 0 || current.y == 0 || current.x == n-1 || current.y == n-1) {
+        printf("At most %d rose(s) trampled.\n",current.max);
+        break;
+      }
+      for (int i = 0;i < 4;i++) {
+        state temp = current;
+        temp.x += d[i][0];
+        temp.y += d[i][1];
+        if (map[temp.x][temp.y] == 'R') temp.up ++;
+        if (map[n-1-temp.y][temp.x] == 'R') temp.left ++;
+        if (map[n-1-temp.x][n-1-temp.y] == 'R') temp.down++;
+        if (map[temp.y][n-1-temp.x] == 'R') temp.right++;
+        temp.max = max(temp.up,temp.right);
+        temp.max = max(temp.max,temp.down);
+        temp.max = max(temp.max,temp.left);
+        if (!visit[temp.x][temp.y][temp.up][temp.right][temp.down][temp.left]) {
+          visit[temp.x][temp.y][temp.up][temp.right][temp.down][temp.left] = true;
+          pq.push(temp);
+        }
+      }
+    }
+  }
+}
