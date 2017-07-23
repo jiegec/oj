223641df73fd9c8a060506e52eb3fea3f78bc909
Jiege Chen <jiegec@qq.com>
2017-07-23 16:16:34 +0800

    Fix hdu1501 hdu2571

---
+++ hdu/hdu1501.cpp
@@ -0,0 +1,40 @@
+#include <cstdio>
+#include <cstdlib>
+#include <cstring>
+
+char first[210],second[210],third[210];
+bool visit[210][210];
+int firstlen,secondlen,thirdlen;
+bool dfs(int firstx, int secondx, int thirdx) {
+  if (thirdx == thirdlen) {
+    return true;
+  }
+  if(visit[firstx][secondx])
+    return false;
+  visit[firstx][secondx] = true;
+  if(first[firstx]==third[thirdx]) {
+    if(dfs(firstx+1,secondx,thirdx+1))
+      return true;
+  }
+  if(second[secondx]==third[thirdx]){
+    if(dfs(firstx,secondx+1,thirdx+1))
+      return true;
+  }
+  return false;
+}
+int main() {
+  int n;
+  scanf("%d",&n);
+  for(int i = 1;i <= n;i++) {
+    memset(visit,0,sizeof(visit));
+    scanf("%s %s %s",first,second,third);
+    firstlen = strlen(first);
+    thirdlen = strlen(third);
+    secondlen = strlen(second);
+    if(dfs(0,0,0)) {
+      printf("Data set %d: yes\n",i);
+    }else {
+      printf("Data set %d: no\n",i);
+    }
+  }
+}
--- hdu/hdu2571.cpp
+++ hdu/hdu2571.cpp
@@ -7,9 +7,9 @@ int max3(int a, int b, int c) {
 	return max2(a,max2(b,c));
 }
 
-void print(int dp[21][1001], int n, int m) {
-	for(int i = 0;i <= n;i++) {
-		for(int j = 0;j <= m;j++) {
+void print(int dp[25][1005], int n, int m) {
+	for(int i = 1;i <= n;i++) {
+		for(int j = 1;j <= m;j++) {
 			printf("%-6d ",dp[i][j]);
 		}
 		printf("\n");
@@ -28,6 +28,7 @@ int main() {
 		for(int i = 1;i <= n;i++) {
 			for(int j = 1;j <= m;j++) {
 				scanf("%d",&numbers[i][j]);
+        dp[i][j] = -99999;
 			}
 		}
 		for(int i = 0;i <= n;i++) {
@@ -36,20 +37,18 @@ int main() {
 		for(int i = 0;i <= m;i++) {
 			dp[0][i] = -99999;
 		}
-		dp[1][1] = numbers[1][1];
+		dp[1][1] = 0;
 		for(int i = 1;i <= n;i++) {
-			for(int j = 1;j <= m;j++) {				 
-				dp[i][j] = 0;
-				dp[i][j] = max3(dp[i][j],dp[i-1][j],dp[i][j-1]);
-				for(int k = 1;k < j;k++) {
-					if(j % k == 0) {
-						dp[i][j] = max2(dp[i][j],dp[i][k]);
-					}
-				}
-				dp[i][j] += numbers[i][j];
+			for(int j = 1;j <= m;j++) {
+        dp[i][j] += numbers[i][j];
+        dp[i][j+1] = max2(dp[i][j+1],dp[i][j]);
+        dp[i+1][j] = max2(dp[i+1][j],dp[i][j]);
+        for(int k = j<<1;k <= m;k += j)
+          dp[i][k] = max2(dp[i][k],dp[i][j]);
 				//print(dp,n,m);
 			}
 		}
+    // print(dp,n,m);
 		printf("%d\n",dp[n][m]);
 	}
 }
